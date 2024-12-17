2bd09f9094e0cbd34a865730c3a0584361c57081
Mason Schafer <mason.schafer1@gmail.com>
2024-12-17 00:48:08 -0500

    chore: env types

---
+++ reel-journal-backend/globals.d.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+namespace NodeJS {
+  interface ProcessEnv {
+    TMDB_API_KEY: string;
+    FRONTEND_URL: string;
+    PORT: string;
+    JWT_SECRET: string;
+  }
+}
